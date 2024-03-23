c228685249da60e05ed28ac67609f005772b5289
Janaye Jackson <97257796+JanayeJackson@users.noreply.github.com>
2024-03-22 22:15:48 -0700

    Add files via upload

---
+++ Project/Connect 4/Game v2/Board.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   Board.h
+ * Author: Janaye Jackson
+ *
+ * Created on March 22, 2024, 9:00 p.m
+ */
+
+#ifndef BOARD_H
+#define BOARD_H
+
+struct BOARD
+{
+    int row, col;
+    int **game; // a standard connect four game is 6 x 7
+};
+
+#endif /* BOARD_H */
+
